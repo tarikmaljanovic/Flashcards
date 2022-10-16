b36c5392c7192958bd65faf9e5fcd5b42327c01b
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2022-10-16 16:59:36 +0200

    Flashcards

--- study.js
+++ study.js
@@ -2,8 +2,8 @@ const select = document.querySelector("#decks");
 const shuffle = document.querySelector(".shuffle");
 const currentDeck = document.querySelector(".current-deck");
 const card = document.querySelector(".card");
-const front = document.querySelector(".front");
-const back = document.querySelector(".back");
+const cardFront = document.querySelector(".front");
+const cardBack = document.querySelector(".back");
 
 for(let i = 0; i < localStorage.length; i++)  {
   let key = localStorage.key(i);
@@ -11,7 +11,23 @@ for(let i = 0; i < localStorage.length; i++)  {
   select.options.add(new Option(key));
 }
 
-shuffle.addEventListener('click', function(event) {
+function shuffleArray(array) {
+  for(let i = array.length-1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  
+  return array;
+}
 
+
+shuffle.addEventListener('click', function() {
+  let studyDeck = shuffleArray(JSON.parse(localStorage.getItem(select.value)));
+
+  let card = studyDeck[0];
+  cardFront.innerHTML = String(Object.keys(card));
+  cardBack.innerHTML = String(Object.values(card));
 });
 
